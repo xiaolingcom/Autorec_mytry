c7a49f08a57eba001aff4b55bbc6fd4cb342b0fc
lxling <1580336289@qq.com>
2019-11-20 15:51:33 +0800

    update

--- autoRec.py
+++ autoRec.py
@@ -27,7 +27,9 @@ class autoEncoder():
         self.curEpoch = 0
         #画图所要用到的数据
         self.train_losses=[]
+        self.train_RMSEs=[]
         self.test_losses=[]
+        self.test_RMSEs=[]
         self.step_losses=[]
 
     def prePareModel(self):
@@ -42,7 +44,7 @@ class autoEncoder():
         self.W = t.empty(LATENT_DIM,D_out,dtype=t.float,device=device,requires_grad=True)
         self.b1=t.zeros(LATENT_DIM,dtype=t.float,device=device,requires_grad=True)
         self.b2=t.zeros(D_out,dtype=t.float,device=device,requires_grad=True)
-        nn.init.xavier_uniform_(self.V) #///////////////////////////////////////////////?
+        nn.init.xavier_uniform_(self.V) 
         nn.init.xavier_uniform_(self.W)
         self.optimizer=t.optim.Adam([self.V,self.W,self.b1,self.b2],lr=self.LR)
 
@@ -58,37 +60,46 @@ class autoEncoder():
         shuffledIds=np.random.permutation(num)
         steps=int(np.ceil(num/BATCH_SIZE)) #没有整除，取上值
         epoch_loss=0
+        epoch_rmse_loss=0
+        epoch_rmse_num=0
         for i in range(steps):
-            ed=min((i+1)*BATCH_SIZE,num) #最小,不是最大
+            ed=min((i+1)*BATCH_SIZE,num) 
             batch_Ids=shuffledIds[i*BATCH_SIZE:ed]
             batch_len=len(batch_Ids) #后面计算batch_rmse需要用到
 
-            #第i步训练所用的数据
+            #准备第i步训练所用的数据
             ##转换为numpy格式
-            tmpTrian=trainMat[batch_Ids].toarray()
+            tmpTrain=trainMat[batch_Ids].toarray()
             tmpMask=trainMask[batch_Ids].toarray()
             ##转换为tensor格式
-            train=t.from_numpy(tmpTrian).float().to(device)
+            train=t.from_numpy(tmpTrain).float().to(device)
             mask=t.from_numpy(1*tmpMask).float().to(device)
 
             y_pred=self.model(train) #第i步所用数据的预测值
-            #计算误差(目标函数)
-            pred_loss=self.loss_mse(y_pred*mask,train)/batch_len
+            #目标函数
+            pred_loss=self.loss_mse(y_pred*mask,train)/batch_len #为什么这里要除一个batchsize呢，原文的损失函数好像没有除
             v_loss=t.sum(self.V*self.V) #t.sum而不是t.mm
             w_loss=t.sum(self.W*self.W) #*是element-wise操作
             batch_loss=pred_loss + self.V_regularWeight*v_loss +self.W_regularWeight*w_loss
+            #计算误差rmse
             epoch_loss+=batch_loss.item()
+            epoch_rmse_loss+=self.RMSE(y_pred.cpu().detach().numpy(),tmpTrain,tmpMask)#//////////////////②?????/////////////
+            epoch_rmse_num+=t.sum(mask).item()
+
             log('step %d/%d, step_loss=%f'%(i,steps,batch_loss.item()),save=False,oneline=True)
             op.zero_grad() #清空梯度，不累加
             batch_loss.backward()
             op.step()
-        return epoch_loss
+        epoch_rmse=np.sqrt(epoch_rmse_loss/epoch_rmse_num)
+        return epoch_loss,epoch_rmse
 
     def testModel(self,trainMat,testMat,testMask,div):
         num=trainMat.shape[0]
         shuffledIds=np.random.permutation(num)
         steps=int(np.ceil(num/(BATCH_SIZE*div)))
         epoch_loss=0
+        epoch_rmse_loss=0
+        epoch_rmse_num=0
         for i in range(steps):
             ed=min((i+1)*BATCH_SIZE,num)
             batch_Ids=shuffledIds[i*BATCH_SIZE:ed]
@@ -105,13 +116,11 @@ class autoEncoder():
 
             y_pred=self.model(train)
             #用测试集的误差去衡量泛化误差,这个时候的参数应该训练好了
-            pred_loss=self.loss_mse(y_pred*mask,test)/batch_len
-            v_loss=t.sum(self.V*self.V)
-            w_loss=t.sum(self.W*self.W)
-            batch_loss=pred_loss+self.V_regularWeight*v_loss+self.W_regularWeight*w_loss
-            epoch_loss+=batch_loss.item()  #item()将tensor转成一个数
-
-        return epoch_loss
+            epoch_rmse_loss+=self.RMSE(y_pred.cpu().detach().numpy(),tmpTest,tmptestMask)
+            epoch_loss+=epoch_rmse_loss #/////////////???/////////////
+            epoch_rmse_num+=t.sum(mask).item()
+        epoch_rmse=np.sqrt(epoch_rmse_loss/epoch_rmse_num)
+        return epoch_loss,epoch_rmse
 
     def run(self):
         self.prePareModel()
@@ -119,17 +128,19 @@ class autoEncoder():
         #     self.loadModel(LOAD_MODEL_PATH)
         #训练
         for e in range(self.curEpoch,EPOCH+1):
-            epoch_loss=self.trainModel(self.trainMat,self.trainMask,self.optimizer)
-            log("epoch %d/%d, epoch_loss=%.2f"%(e,EPOCH,epoch_loss))
+            epoch_loss,epoch_rmse=self.trainModel(self.trainMat,self.trainMask,self.optimizer)
+            log("epoch %d/%d, epoch_loss=%.2f, epoch_rmse=%.4f"%(e,EPOCH,epoch_loss,epoch_rmse))
             self.train_losses.append(epoch_loss)
+            self.train_RMSEs.append(epoch_rmse)
             #打印正则损失
             log('V_Loss = %.2f, W_Loss = %.2f'%(t.sum(self.V * self.V), t.sum(self.W * self.W)))
 
             #交叉验证
-            cv_epoch_loss=self.testModel(self.trainMat,self.testMat,self.testMask,5)
-            log("epoch %d/%d, cv_epoch_loss=%.2f"%(e, EPOCH, cv_epoch_loss))
+            cv_epoch_loss,cv_epoch_rmse=self.testModel(self.trainMat,self.testMat,self.testMask,5)#//////////①???///////////
+            log("epoch %d/%d, cv_epoch_loss=%.2f,cv_epoch_rmse=%.4f"%(e, EPOCH, cv_epoch_loss,cv_epoch_rmse))
             log("\n")
             self.test_losses.append(cv_epoch_loss)
+            self.test_RMSEs.append(cv_epoch_rmse)
 
             #调整学习率并保存
             self.curLr = self.adjust_learning_rate(self.optimizer, e)
@@ -144,17 +155,17 @@ class autoEncoder():
             #     #     print("rmse = %.4f"%(self.step_rmse[i]))
             #     #     print("***************************")
         #测试
-        test_epoch_loss=self.testModel(self.trainMat,self.testMat,self.testMask,1)
-        self.writeResult(test_epoch_loss)
+        _,test_rmse=self.testModel(self.trainMat,self.testMat,self.testMask,1)
+        self.writeResult(test_rmse)
         log("\n")
-        log("test_epoch_loss=%f"%(test_epoch_loss)) #rmse没有数出来，最终的评估指标
+        log("test_rmse=%.4f"%(test_rmse))
         self.getModelName()
 
     #根据epoch数调整学习率
     def adjust_learning_rate(self, optimizer, epoch):
         LR = self.LR * (LR_DECAY**epoch)
         for param_group in optimizer.param_groups:
-            param_group['lr'] = LR #保存//////////////////////////////
+            param_group['lr'] = LR #保存////////////3//////////////////
         return LR
 
     def saveModel(self):
@@ -200,6 +211,9 @@ class autoEncoder():
             f.write(modelName + '\r\n')
             f.write(str(result) + '\r\n')
 
+    def RMSE(self, decoder, label, mask): #预测数据和输入数据的square error
+            add_avg = decoder + 3 - 3 * (decoder != 0) #没有评分的统一给3分
+            return np.sum(np.square((add_avg - label) * mask))
 #####################################################################################
 if __name__=='__main__':
     params = sys.argv
